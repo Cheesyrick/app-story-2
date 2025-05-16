1c8ec0321110d26c4db518fe4af85183af474a14
cheeserickey@gmail.com <cheeserickey@gmail.com>
2025-05-16 22:58:07 +0700

    added PWA feature

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -54,3 +54,16 @@ window.addEventListener("keydown", (e) => {
     document.body.classList.add("user-is-tabbing");
   }
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+      .then((registration) => {
+        console.log('Service Worker registered successfully:', registration);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
+
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -28,8 +29,33 @@ module.exports = merge(common, {
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-  ],
+plugins: [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  new WorkboxWebpackPlugin.GenerateSW({
+    swDest: 'sw.js', // Nama file Service Worker hasil build
+    clientsClaim: true,
+    skipWaiting: true,
+    runtimeCaching: [
+      {
+        urlPattern: /\.(?:png|jpg|jpeg|svg|gif|woff2?|eot|ttf|otf)$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'static-resources',
+        },
+      },
+      {
+        urlPattern: /^https:\/\/story-api\.dicoding\.dev\/v1\//,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'api-cache',
+          networkTimeoutSeconds: 3,
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  }),
+],
 });
